e3a6c515468474db71be3e7ad90df4c611d48846
박도희 <dohee.pa@gmail.com>
2020-09-22 12:38:17 +0900

    UPDATE | math

--- 13458.py
+++ 13458.py
@@ -1,14 +1,13 @@
 # BOJ 13458 시험 감독
 
+from math import ceil
+
+
 N = int(input())
 A = list(map(int, input().split()))
 B, C = map(int, input().split())
-count = 0
-for n in range(N):
-    A[n] = max(A[n] - B, 0)
-    count += 1
-    if A[n]:
-        count += A[n] // C
-        if A[n] % C:
-            count += 1
-print(count)
+answer = N
+for students in A:
+    if students > B:
+        answer += ceil((students - B) / C)
+print(answer)
