69b0d9aa6b3e8e0d03304dda180dd90a8ae71b96
박도희 <dohee.pa@gmail.com>
2020-12-09 22:48:15 +0900

    UPDATE | set

--- 7785.py
+++ 7785.py
@@ -1,14 +1,14 @@
 # 7785 회사에 있는 사람
 
+from sys import stdin
+
 name, access = 0, 1
 company = set()
-N = int(input())
+N = int(stdin.readline())
 for tc in range(N):
-    log = list(map(str, input().split()))
+    log = list(map(str, stdin.readline().split()))
     if log[access] == 'enter':
         company.add(log[name])
     else:
         company.remove((log[name]))
-company = sorted(list(company), reverse=True)
-for staff in company:
-    print(staff)
+print('\n'.join(sorted(list(company), reverse=True)))
